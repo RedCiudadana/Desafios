40485b06baeed88405b70736e54543180fa3d381
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-10-11 11:13:23 -0600

    toques finales

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
-const rmj = require('render-markdown-js')
+const moment = require("moment");
+const MarkdownIt = require('markdown-it');
+const now = new Date();
+const rmj = require('render-markdown-js');
 
 module.exports = function (eleventyConfig) {
 
@@ -7,6 +10,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src');
     eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy('images');
 
     eleventyConfig.addNunjucksFilter("rmj", function(content) {
         return rmj(content);
@@ -14,5 +18,9 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addNunjucksFilter("limit", function (array, limit) {
         return array.slice(0, limit);
+    });
+
+    eleventyConfig.addFilter("dateFormat", function(date, format) {
+        return moment(date).format(format);
     });
 }
